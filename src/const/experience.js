28b3de966c9cdd73b2300730f7ed5d3d5d9ff6af
Sergey Chystiakov <schystiakov@ngoar.com>
2016-08-07 13:48:02 +0300

    Update 2016-08-07T10:47:59.016Z

--- src/const/experience.js
+++
@@ -1,7 +0,0 @@
-export const EXPERIENCE = {
-  1: { label: '1 год', k: 1 },
-  2: { label: '2 года', k: 1 },
-  3: { label: '3 года', k: 1 },
-  4: { label: '4 года', k: 1 },
-  5: { label: '5 лет+', k: 1 },
-};
